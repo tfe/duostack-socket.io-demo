af53eb6bf2a1c5e639f0cbe8f9acde765b9f79a2
Todd Eichel <todd@toddeichel.com>
2011-01-22 18:29:26 -0500

    Add a template HTML file, which we'll fill in with updates from the server via Socket.IO.

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8124, '127.0.0.1');
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFile('demo.html', function (err, data) {
+    if (err) throw err;
+    res.end(data);
+  });
+});
 
+server.listen(8124, '127.0.0.1');
 console.log('Server started');
