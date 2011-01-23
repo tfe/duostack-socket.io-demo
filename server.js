6ee42cba36012c7a25184967e30d327f2e65d7a5
Todd Eichel <todd@toddeichel.com>
2011-01-23 03:22:52 -0500

    Add Socket.IO to template HTML and Node server.js. Client should connect immediately, then await messages from the server with the current server time.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http'),
-    fs = require('fs');
+    fs = require('fs'),
+    io = require('socket.io');
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
@@ -11,3 +12,21 @@ var server = http.createServer(function (req, res) {
 
 server.listen(8124, '127.0.0.1');
 console.log('Server started');
+
+
+// socket.io stuff
+/////////////////////////////////////////////////////////////////////////////
+
+// set some config options for Duostack: no flash policy server, no websocket or flashsocket transports
+var socket = io.listen(server, {
+  flashPolicyServer: false,
+  transports: ['htmlfile', 'xhr-multipart', 'xhr-polling', 'jsonp-polling']
+});
+
+// fires on every new client connection
+socket.on('connection', function (client) {
+  // the following sets up an interval to send the current time to the client every 1000ms
+  setInterval(function () {
+    client.send(new Date());
+  }, 1000)
+});
